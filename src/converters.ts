c6c3c0eb50b547494730e17e0bdf678a72de8cf8
Teun Buijs <teunianbuijs@gmail.com>
2023-09-04 00:16:13 +0200

    added a custom, an auto, and a multi converter

--- src/converters.ts
+++ src/converters.ts
@@ -4,6 +4,7 @@ export const COMMAND_ID = "extension.tracyCsvConverter";
 // Make this something unique
 export const SCHEME = 'vscodeTracyCsvConverter';
 
+
 // Define converters here
 function standardConvert(content: string) {
 	const rows = content.split('\n').filter((l) => l.trim() !== '').map((l) => l.split(','));
@@ -15,7 +16,113 @@ function standardConvert(content: string) {
 	});
 }
 
+// Define your own converter
+export const COL_DELIMITERS: {[s: string]: string} = {
+	'Comma ","'    	: ',',
+	'Semicolon ";"'	: ';',
+	'Colon ":"'		: ':',
+	'Tab "\t"'		: '\t',
+	'Line "|"'		: '|',
+};
+export const ROW_DELIMITERS: {[s: string]: string} = {
+	'Newline "\n"'  		: '\n', // Unix and unix-like (also works for windows I believe because that uses \r\n)
+	'Carriage Return "\r"'	: '\r', // old apple computers
+	'Tab "\t"'				: '\t',
+}
+function customSingleConverter(content: string, col_delimiter: string = ',', row_delimiter: string = '\n', sort_by_column: string | undefined) {
+	const rows = content.split(row_delimiter) // split by row delimiter
+		.filter((l) => l.trim() !== '') // remove leading and trailing whitespace
+		.map((l) => l.split(col_delimiter)); // split by column delimiter and copy to new array
+	const headers = rows[0];
+	return rows.slice(1).map((r) => {
+		const row: {[s: string]: string} = {};
+		headers.forEach((h, i) => row[h] = r[i]);
+		return row;
+	}).sort((a: {[s: string]: string}, b: {[s: string]: string}) => {
+		if (sort_by_column) // TODO: its a bit inefficient to have the if call inside the sort, fix it
+			return a[sort_by_column] > b[sort_by_column] ? 1 : -1;
+		else 
+			return 0;
+	});
+}
+
+export function getHeaders(content: string, col_delimiter: string = ',') { // only expect the first line
+	return content.split(col_delimiter);
+}
+
+export function getColumnDelimiter(content: string, row_delimiter: string = '\n') {
+	// this function assumes that the row delimiter is a newline
+	// will start simple by checking the amount of chars of each row and checking if they are the same
+	const rows = content.slice(0, content.lastIndexOf(row_delimiter)) // ensure only checking complete rows
+		.split(row_delimiter).filter((l)=> l.trim() !== '');
+	
+	const char_counts = rows.map((row: string) => {
+		// get the char count of a row
+		let count: {[s: string]: number} = {};
+		[...row].forEach(char => {
+			if (!(char in count)) count[char] = 0;
+			count[char] = count[char] + 1;
+		});
+		return count;
+	});
+
+	const shared_char_counts = char_counts.reduce((prev_row, current_row) => {
+		let intersect_chars: {[s: string]: number} = {};
+		for (const char in prev_row) {
+			if (current_row[char] === prev_row[char]) intersect_chars[char] = prev_row[char]; // keep only the chars that both rows have
+		}
+		return intersect_chars;
+	});
+	// console.log("Found delimiters: ", shared_char_counts);
+	const shared_chars = Object.keys(shared_char_counts);
+	if (shared_chars.length > 0) return shared_chars[0];
+	// TODO: ask user what delimiter to use (show example text maybe?)
+	return ','; // if no shared character is found, return the default delimiter
+}
+
+function autoCSVConverter(content: string): {[s: string]: string}[] {
+	const col_delimiter = getColumnDelimiter(content.slice(0, 5000));
+	const rows = content.split('\n').filter((l) => l.trim() !== '').map((l) => l.split(col_delimiter));
+	const headers = rows[0];
+	return rows.slice(1).map((r) => {
+		const row: {[s: string]: string} = {};
+		headers.forEach((h, i) => row[h] = r[i]);
+		return row;
+	});
+}
+
 // List all converters here, key will be the name, value is the converter function
-export const CONVERTERS: {[s: string]: (content: string) => {[s: string]: string}[]} = {
-	'Using standard converter' : standardConvert,
+export const CONVERTERS: {[s: string]: (content: string, ...args: any[]) => {[s: string]: string}[]} = {
+	'Using standard converter.' : standardConvert,
+	'Auto converter.'			: autoCSVConverter,
+	'Define custom converter!' 	: customSingleConverter,
+}
+
+function string_compare(a: string, b: string) {
+	return a < b; // default comparator, currently only works because the timestamps are reasonably structured
+}
+
+export function multiConverter(files: string[], sort_column: string = "timestamp", comparator: (a: string, b: string) => boolean = string_compare) : {[s: string]: string}[] {
+	// TODO: add multiple different headers options
+	const tracy_docs = files.map(content => autoCSVConverter(content));
+	// combine the files
+	return tracy_docs.reduce((prev, current) => {
+		// assumption is that the "timestamp"s are already sorted in both prev and current
+		// this means an insertion sort/merge is efficient
+		let prev_index = 0;
+		let current_index = 0;
+		let output: {[s: string]: string}[] = [];
+		while (prev_index < prev.length || current_index < current.length) {
+			if (prev_index === prev.length) output.push(current[current_index++]);
+			else if (current_index === current.length) output.push(prev[prev_index++]);
+			else if (comparator(prev[prev_index][sort_column], current[current_index][sort_column])) {
+				output.push(prev[prev_index]);
+				prev_index++;
+			} else {
+				output.push(current[current_index]);
+				current_index++;
+			}
+		}
+		return output;
+	});
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 'use strict';
 
 import * as vscode from 'vscode';
-import { COMMAND_ID, SCHEME, CONVERTERS } from './converters';
+import * as converters from './converters'; // might want to change into a *
 
 const TRACY_EDITOR = 'tno.tracy';
 
@@ -17,20 +17,63 @@ export function activate(context: vscode.ExtensionContext) {
 			return content;
 		}
 	};
-	context.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider(SCHEME, provider));
+	context.subscriptions.push(vscode.workspace.registerTextDocumentContentProvider(converters.SCHEME, provider));
 
-	const disposable = vscode.commands.registerCommand(COMMAND_ID, async () => {
+	const multiConverterCommand = vscode.commands.registerCommand("extension.tracyMultiCsvConverter", async () => {
+		vscode.window.showOpenDialog({ canSelectMany: true, openLabel: "Open", canSelectFolders: false }).then(fileURIs => {
+			if (fileURIs) {
+				//console.log("There are %d files selected.", fileURI.length);
+				const new_file_uri = vscode.Uri.parse(`${converters.SCHEME}:multiparsed.tracy.json`);
+
+				Promise.all(fileURIs.map(async file => { // wait for all files to be read
+					return (await vscode.workspace.openTextDocument(file)).getText(); // TODO: find whether documents are opened as streams
+				})).then(async files_contents => {
+					// console.log("Found %d files' contents", files_contents.length);
+					const converted = converters.multiConverter(files_contents); // TODO: add the sorted column and the comparator
+					// console.log("Converted the selected file(s), string length %d", converted.length);
+					contents[new_file_uri.path] = JSON.stringify(converted);
+					await vscode.commands.executeCommand('vscode.openWith', new_file_uri, TRACY_EDITOR);
+				});
+			}
+		});
+		
+	});
+	context.subscriptions.push(multiConverterCommand);
+
+	const disposable = vscode.commands.registerCommand(converters.COMMAND_ID, async () => {
 		const editor = vscode.window.activeTextEditor;
 		if (editor) {
-			const choice = await vscode.window.showQuickPick(Object.keys(CONVERTERS));
+			const choice = await vscode.window.showQuickPick(Object.keys(converters.CONVERTERS));
 			if (choice) {
-				const converter = CONVERTERS[choice];
-				const uri = vscode.Uri.parse(`${SCHEME}:${editor.document.fileName}`);
-				const converted = converter(editor.document.getText());
-				contents[uri.path] = JSON.stringify(converted);
-				await vscode.commands.executeCommand('vscode.openWith', uri, TRACY_EDITOR);
+				const converter = converters.CONVERTERS[choice];
+				const uri = vscode.Uri.parse(`${converters.SCHEME}:${editor.document.fileName.replace(".csv", ".tracy.json")}`);
+				if (choice.endsWith("!")) { // TODO: Basic identifier, change to a more sophisticated method of determining whether special actions have to be taken
+					// Define-your-own / DIY csv converter
+					getCSVInfo(editor.document).then(async csvInfo => {
+						const converted = converter(editor.document.getText(), converters.COL_DELIMITERS[csvInfo[1]], converters.ROW_DELIMITERS[csvInfo[0]], csvInfo[2]); // this gets the document from that is currently open in the editor
+						// what I want is to be able to select multiple files not in the editor
+						contents[uri.path] = JSON.stringify(converted);
+						await vscode.commands.executeCommand('vscode.openWith', uri, TRACY_EDITOR);
+					});					
+				} else {
+					const converted = converter(editor.document.getText());
+					contents[uri.path] = JSON.stringify(converted);
+					await vscode.commands.executeCommand('vscode.openWith', uri, TRACY_EDITOR);
+				}
+				//await vscode.commands.executeCommand('vscode.openWith', uri, TRACY_EDITOR);
 			}
 		}
 	});
 	context.subscriptions.push(disposable);
+}
+
+async function getCSVInfo(doc: vscode.TextDocument) : Promise<[string, string, string | undefined]> { // Use this function to ask the user what format the csv file is in
+	const row_delimiter = await vscode.window.showQuickPick(Object.keys(converters.ROW_DELIMITERS), {title: "What symbol divides the rows?"});
+	if (!row_delimiter) return Promise.reject(); // user has cancelled the operation
+	const col_delimiter = await vscode.window.showQuickPick(Object.keys(converters.COL_DELIMITERS), {title: "What symbol divides the columns?"});
+	if (!col_delimiter) return Promise.reject(); // user has cancelled the operation
+	const headers = converters.getHeaders(doc.lineAt(0).text, converters.COL_DELIMITERS[col_delimiter]);
+	// TODO: currently there is no way to cancel the operation at this stage, maybe change it so that it is possible
+	const sort_column = await vscode.window.showQuickPick(headers, { title: "Sort by which column? (Esc for no sorting)" }); // undefined means no sorting
+	return [row_delimiter, col_delimiter, sort_column];
 }
