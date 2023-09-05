ab35ab9399884bf390aa4be30551b3dbc78852a1
Teun Buijs <t.i.buijs@student.tudelft.nl>
2023-09-05 10:00:16 +0200

    prettified converters, added comments

--- src/converters.ts
+++ src/converters.ts
@@ -34,22 +34,23 @@ function customSingleConverter(content: string, col_delimiter: string = ',', row
 		.filter((l) => l.trim() !== '') // remove leading and trailing whitespace
 		.map((l) => l.split(col_delimiter)); // split by column delimiter and copy to new array
 	const headers = rows[0];
-	return rows.slice(1).map((r) => {
+	const to_be_sorted = rows.slice(1).map((r) => {
 		const row: {[s: string]: string} = {};
 		headers.forEach((h, i) => row[h] = r[i]);
 		return row;
-	}).sort((a: {[s: string]: string}, b: {[s: string]: string}) => {
-		if (sort_by_column) // TODO: its a bit inefficient to have the if call inside the sort, fix it
-			return a[sort_by_column] > b[sort_by_column] ? 1 : -1;
-		else 
-			return 0;
 	});
+	if (sort_by_column) return to_be_sorted.sort((a: {[s: string]: string}, b: {[s: string]: string}) => {
+		return a[sort_by_column] > b[sort_by_column] ? 1 : -1;
+	});
+	return to_be_sorted;
 }
 
-export function getHeaders(content: string, col_delimiter: string = ',') { // only expect the first line
-	return content.split(col_delimiter);
-}
-
+/**
+ * Returns a likely candidate for a CSV file's column delimiter.
+ * @param content A slice of CSV content that contains multiple rows. The bigger the slice, the more accurate this function becomes.
+ * @param row_delimiter The row delimiter, default='\n'.
+ * @returns A char which is occurs the same amount of times in each row.
+ */
 export function getColumnDelimiter(content: string, row_delimiter: string = '\n') {
 	// this function assumes that the row delimiter is a newline
 	// will start simple by checking the amount of chars of each row and checking if they are the same
@@ -66,6 +67,7 @@ export function getColumnDelimiter(content: string, row_delimiter: string = '\n'
 		return count;
 	});
 
+	// only keep the chars that are present in each row, and occur the same amount of times
 	const shared_char_counts = char_counts.reduce((prev_row, current_row) => {
 		let intersect_chars: {[s: string]: number} = {};
 		for (const char in prev_row) {
@@ -75,13 +77,14 @@ export function getColumnDelimiter(content: string, row_delimiter: string = '\n'
 	});
 	// console.log("Found delimiters: ", shared_char_counts);
 	const shared_chars = Object.keys(shared_char_counts);
-	if (shared_chars.length > 0) return shared_chars[0];
-	// TODO: ask user what delimiter to use (show example text maybe?)
+	if (shared_chars.length > 0) return shared_chars[0]; // TODO: is there a better way to get the option than taking the first char?
+	// TODO: ask user what delimiter to use (show example text maybe?), instead of giving a default
 	return ','; // if no shared character is found, return the default delimiter
 }
 
 function autoCSVConverter(content: string): {[s: string]: string}[] {
-	const col_delimiter = getColumnDelimiter(content.slice(0, 5000));
+	// Only use a slice of the string to compute the column delimiter, on account of efficiency
+	const col_delimiter = getColumnDelimiter(content.slice(0, Math.min(5000, content.length)));
 	const rows = content.split('\n').filter((l) => l.trim() !== '').map((l) => l.split(col_delimiter));
 	const headers = rows[0];
 	return rows.slice(1).map((r) => {
@@ -93,16 +96,16 @@ function autoCSVConverter(content: string): {[s: string]: string}[] {
 
 // List all converters here, key will be the name, value is the converter function
 export const CONVERTERS: {[s: string]: (content: string, ...args: any[]) => {[s: string]: string}[]} = {
-	'Using standard converter.' : standardConvert,
-	'Auto converter.'			: autoCSVConverter,
-	'Define custom converter!' 	: customSingleConverter,
+	'Using standard converter' 	: standardConvert,
+	'Auto converter'			: autoCSVConverter,
+	'Define custom converter' 	: customSingleConverter,
+}
+export const COMPARATORS: {[s: string]: (a: string, b: string) => number} = {
+	"String compare"		: (a: string, b: string) => a.localeCompare(b),
+	"Date compare"			: (a, b) => (new Date(a).getTime() - new Date(b).getTime()),
 }
 
-function string_compare(a: string, b: string) {
-	return a < b; // default comparator, currently only works because the timestamps are reasonably structured
-}
-
-export function multiConverter(files: string[], sort_column: string = "timestamp", comparator: (a: string, b: string) => boolean = string_compare) : {[s: string]: string}[] {
+export function multiConverter(files: string[], sort_column: string = "timestamp", comparator: (a: string, b: string) => number = COMPARATORS["String compare"]) : {[s: string]: string}[] {
 	// TODO: add multiple different headers options
 	const tracy_docs = files.map(content => autoCSVConverter(content));
 	// combine the files
@@ -115,7 +118,7 @@ export function multiConverter(files: string[], sort_column: string = "timestamp
 		while (prev_index < prev.length || current_index < current.length) {
 			if (prev_index === prev.length) output.push(current[current_index++]);
 			else if (current_index === current.length) output.push(prev[prev_index++]);
-			else if (comparator(prev[prev_index][sort_column], current[current_index][sort_column])) {
+			else if (comparator(prev[prev_index][sort_column], current[current_index][sort_column]) > 0) {
 				output.push(prev[prev_index]);
 				prev_index++;
 			} else {
