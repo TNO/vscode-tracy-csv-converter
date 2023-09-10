57e5c332b52f55cc2ae2c56e009e2853fdab4e81
Teun Buijs <teunianbuijs@gmail.com>
2023-09-10 22:33:15 +0200

    Added simple webview for selecting options

--- src/ConverterReactPanel.ts
+++ src/ConverterReactPanel.ts
@@ -1,37 +1,40 @@
 import vscode from 'vscode';
 
 // A lot of the code here is from https://github.com/rebornix/vscode-webview-react/blob/master/ext-src/extension.ts
-class ReactPanel {
+export class ConverterPanel {
     // track current panel
-    public static currentPanel: ReactPanel | undefined;
+    public static currentPanel: ConverterPanel | undefined;
+
+	private static readonly viewType = 'tracyCSVConverterOptions';
 
     private readonly _panel: vscode.WebviewPanel;
-    private readonly _extensionPath: string;
+    private readonly _extensionUri: vscode.Uri;
 	private _disposables: vscode.Disposable[] = [];
 
-    public static createOrShow(extensionPath: string) {
+    public static createOrShow(extensionUri: vscode.Uri) {
         const column = vscode.window.activeTextEditor ? vscode.window.activeTextEditor.viewColumn : undefined;
 
 		// If we already have a panel, show it.
 		// Otherwise, create a new panel.
-		if (ReactPanel.currentPanel) {
-			ReactPanel.currentPanel._panel.reveal(column);
+		if (ConverterPanel.currentPanel) {
+			ConverterPanel.currentPanel._panel.reveal(column);
 		} else {
-			ReactPanel.currentPanel = new ReactPanel(extensionPath, column || vscode.ViewColumn.One);
+			ConverterPanel.currentPanel = new ConverterPanel(extensionUri, column || vscode.ViewColumn.One);
 		}
     }
 
-    private constructor(extensionPath: string, column: vscode.ViewColumn) {
-		this._extensionPath = extensionPath;
+    private constructor(extensionUri: vscode.Uri, column: vscode.ViewColumn) {
+		this._extensionUri = extensionUri;
 
 		// Create and show a new webview panel
-		this._panel = vscode.window.createWebviewPanel(ReactPanel.viewType, "React", column, {
+		this._panel = vscode.window.createWebviewPanel(ConverterPanel.viewType, "Tracy CSV Converter Options", column, {
 			// Enable javascript in the webview
 			enableScripts: true,
 
-			// And restric the webview to only loading content from our extension's `media` directory.
+			// And restric the webview to only loading content from our extension's `out` directory.
 			localResourceRoots: [
-				vscode.Uri.file(path.join(this._extensionPath, 'build'))
+				vscode.Uri.joinPath(extensionUri, "out"),
+				// vscode.Uri.joinPath(extensionUri, "")
 			]
 		});
 		
@@ -44,11 +47,100 @@ class ReactPanel {
 
 		// Handle messages from the webview
 		this._panel.webview.onDidReceiveMessage(message => {
-			switch (message.command) {
+			console.log("Received message:", JSON.stringify(message));
+			switch (message?.command) {
 				case 'alert':
-					vscode.window.showErrorMessage(message.text);
+					vscode.window.showErrorMessage(message?.text);
+					return;
+				case 'update':
+					this._panel.webview.postMessage({
+
+					});
+					return;
+				case 'add-files':
+					console.log("Try to show open dialog");
+					vscode.window.showOpenDialog({ canSelectMany: true, openLabel: "Open", canSelectFolders: false }).then((files) => { if (files) {
+						// Tell the webview to display the files
+						this._panel.webview.postMessage({
+							command: "add-files",
+							data: files.map((f) => f.path)
+						});
+					}});
 					return;
 			}
 		}, null, this._disposables);
 	}
+
+	public doRefactor() {
+		// Send a message to the webview webview.
+		// You can send any JSON serializable data.
+		this._panel.webview.postMessage({ command: 'refactor' });
+	}
+
+	public dispose() {
+		ConverterPanel.currentPanel = undefined;
+
+		// Clean up our resources
+		this._panel.dispose();
+
+		while (this._disposables.length) {
+			const x = this._disposables.pop();
+			if (x) {
+				x.dispose();
+			}
+		}
+	}
+
+	private _getHtmlForWebview() {
+		// const manifest = require((this._extensionPath, 'build', 'asset-manifest.json'));
+		// const mainScript = manifest['files']['main.js'];
+		// const mainStyle = manifest['files']['main.css'];
+
+		const scriptUri = getUri(this._panel.webview, this._extensionUri, ["out", "Ux", "main.js"]); //vscode.Uri.file(path.join(this._extensionPath, 'build', mainScript));
+		// const stylePathOnDisk = vscode.Uri.file(path.join(this._extensionPath, 'build', mainStyle));
+		// const styleUri = stylePathOnDisk.with({ scheme: 'vscode-resource' });
+
+		// Use a nonce to whitelist which scripts can be run
+		const nonce = getNonce();
+		// in <head> : 
+		// <base href="${vscode.Uri.file(path.join(this._extensionPath, 'build')).with({ scheme: 'vscode-resource' })}/">
+		// <link rel="stylesheet" type="text/css" href="${styleUri}">
+		// <meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src vscode-resource: https:; script-src 'nonce-${nonce}';style-src vscode-resource: 'unsafe-inline' http: https: data:;">
+
+		// in <body> :
+		// 
+
+		return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="utf-8">
+				<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+				<meta name="theme-color" content="#000000">
+				<title>React App</title>
+				
+				
+				
+			</head>
+
+			<body>
+				<noscript>You need to enable JavaScript to run this app.</noscript>
+				<div id="root"></div>
+				
+				<script nonce="${nonce}" src="${scriptUri}"></script>
+			</body>
+			</html>`;
+	}
+}
+
+function getUri(webview: vscode.Webview, extensionUri: vscode.Uri, pathlist: string[]) {
+	return webview.asWebviewUri(vscode.Uri.joinPath(extensionUri, ...pathlist));
+}
+
+function getNonce() {
+	let text = "";
+	const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	for (let i = 0; i < 32; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
 }
--- src/Ux/MultiConverterOptionsWebview.tsx
+++ src/Ux/MultiConverterOptionsWebview.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import * as Mui from '@mui/material';
+import { List, ListItem, ListItemButton, ListItemText } from '@mui/material';
+import { VSCodeButton, VSCodeDataGrid, VSCodeDataGridRow, VSCodeDataGridCell } from '@vscode/webview-ui-toolkit/react';
 
 interface Props {
 
 }
 
 interface State {
-
+    files: string[],
 }
 
 const BACKDROP_STYLE: React.CSSProperties = {
@@ -18,24 +19,78 @@ const innerStyle: React.CSSProperties = {
     paddingLeft: '2px'
 };
 
+/**
+ * This is the Webview that is shown when the user wants to select multiple csv files.
+ */
 export default class MultiConverterOptionsWebview extends React.Component<Props, State> {
+    // @ts-ignore
+    vscode = acquireVsCodeApi();
+    
     constructor(props: Props) {
         super(props);
-        this.state = {};
+        this.state = {
+            files: []
+        };
+
+        this.onMessage = this.onMessage.bind(this); // this is done in tracy, dont know why yet
+        window.addEventListener('message', this.onMessage);
+        this.vscode.postMessage({command: "clear"});
+    }
+
+    onMessage(event: MessageEvent) {
+        const message = event.data;
+        switch (message.command) {
+            case "clear":
+                this.setState({files: []}); // temporary
+                break;
+            case "add-files":
+                this.setState({ files: [...this.state.files, ...message.data]});
+                break;
+        }
+    }
+
+    renderFiles() {
+        return (
+            <VSCodeDataGrid id="files-grid">
+                <VSCodeDataGridRow row-rowType='header'>
+                    <VSCodeDataGridCell cellType='columnheader' gridColumn='1'>File</VSCodeDataGridCell>
+                    <VSCodeDataGridCell cellType='columnheader' gridColumn='2'>Type</VSCodeDataGridCell>
+                </VSCodeDataGridRow>
+                {this.state.files.map((file, index) => (<VSCodeDataGridRow>
+                    <VSCodeDataGridCell gridColumn='1'>{file}</VSCodeDataGridCell>
+                    <VSCodeDataGridCell gridColumn='2'>csv</VSCodeDataGridCell>
+                </VSCodeDataGridRow>))}
+            </VSCodeDataGrid>
+        );
+    }
+
+    doAddFiles(event) {
+        //console.log("Sending message");
+        //this.setState({ files: [...this.state.files, "test"] });
+        this.vscode.postMessage({ command: "add-files" }); // tell extension to show file open dialog
     }
 
     render() {
         return (
             <div style={BACKDROP_STYLE}>
+                <h1>Options</h1>
                 <div className='dialog' style={DIALOG_STYLE}>
                     {/* Put the file adding/removing list here */}
-                    <Mui.List>
-                        <Mui.ListItem disablePadding>
-                            <Mui.ListItemButton>
-                                <Mui.ListItemText primary="Test"/>
-                            </Mui.ListItemButton>
-                        </Mui.ListItem>
-                    </Mui.List>
+                    <h2>Files {}</h2>
+                    {this.renderFiles()}
+                    {/* <List>
+                        <ListItem disablePadding>
+                            <ListItemButton>
+                                <ListItemText primary="Test"/>
+                            </ListItemButton>
+                        </ListItem>
+                        <ListItem disablePadding>
+                            <ListItemButton>
+                                <ListItemText primary="Test2"/>
+                            </ListItemButton>
+                        </ListItem>
+                    </List> */}
+                    <VSCodeButton appearance='secondary' onClick={(e) => this.doAddFiles(e)}>Add</VSCodeButton>
                     {/* Put the file options here */}
                 </div>
             </div>
