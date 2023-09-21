a772d3310bc13d47cc3431c619a984589ead5bbf
Bob VAN HOUT <bob.vanhout@alten.nl>
2023-09-21 11:42:01 +0200

    Implement eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+/* eslint-env node */
+module.exports = {
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+    rules: {
+        "no-useless-escape": "off",
+        "no-mixed-spaces-and-tabs": ["error", "smart-tabs"],
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "@typescript-eslint/naming-convention": [
+          "error",
+          {
+            "selector": "variable",
+            "format": ["camelCase","UPPER_CASE"]
+          }
+        ],
+      }
+}
