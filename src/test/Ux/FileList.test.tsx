6fee416034a38fe8c1e91abe9cc8c58599761a8d
Teun Buijs <teunianbuijs@gmail.com>
2023-10-25 17:03:08 +0200

    one works the other doesn't, Im losing my mind

---
+++ src/test/Ux/FileList.test.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { render } from "@testing-library/react";
+// import ReactDOM from "react-dom";
+import { describe, it } from "mocha";
+import { assert, expect } from "chai";
+import FileList from "../../Ux/FileList";
+
+describe("Typescript test example", () => {
+    it("test1", async () => {
+        const ret = render(<button>Save</button>);
+        
+        // const button = await ret.getByLabelText("Save");
+        // expect(button).to.not.be.null;
+
+        const buttonDom = await ret.getByRole("button");
+        expect(buttonDom).to.be.not.null;
+
+        ret.unmount();
+    });
+})
+
+describe("React FileList", () => {
+    it("Renders", async () => {
+        const rendered = render(<FileList files={{}} setFiles={() => {}}/>);
+
+        const title = rendered.getByText("Files");
+        assert.isNotNull(title);
+
+        rendered.unmount();
+    });
+});
--- src/test/index.ts
+++ src/test/index.ts
@@ -1,35 +1,42 @@
 import * as path from 'path';
 import Mocha from 'mocha';
 import { glob } from 'glob';
+import globalJsdom from 'global-jsdom';
 
 // This file is run to execute the tests
 export function run(): Promise<void> {
     // Create the mocha tester
-    const mocha = new Mocha({
+    const mochaPureTS = new Mocha({
         ui: 'tdd',
-        color: true
+        color: true,
+    });
+    
+    mochaPureTS.globalSetup(() => {
+        globalJsdom();
     });
 
     const testsRoot = path.resolve(__dirname, '..');
 
     return new Promise((c, e) => {
-        glob('**/**.test.js', { cwd: testsRoot }).then((files: string[]) => {
+        glob('**/**.test.js?(x)', { cwd: testsRoot }).then((files: string[]) => {
+            
 
-        // Add files to the test suite
-        files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+            // Add files to the test suite
+            files.filter(f => f.endsWith(".test.js")).forEach(f => mochaPureTS.addFile(path.resolve(testsRoot, f)));
 
-        try {
-            // Run the mocha tests
-            mocha.run(failures => {
-            if (failures > 0) {
-                e(new Error(`${failures} tests failed.`));
-            } else {
-                c();
+
+            try {
+                // Run the mocha tests
+                mochaPureTS.run(failures => {
+                    if (failures > 0) {
+                        e(new Error(`${failures} normal tests failed.`));
+                    } else {
+                        c();
+                    }
+                });
+            } catch (err) {
+                e(err);
             }
-            });
-        } catch (err) {
-            e(err);
-        }
         }).catch(error => {
             e(error);
         });
